2087f92f80f8e8b2de39d34a87171da6dbf352e3
Harshitha_7019 <jharshitha28@gmail.com>
2026-02-18 00:13:27 +0530

    Force Connect: Triggering full Netlify rebuild and connecting API paths

--- server/functions/api.js
+++ server/functions/api.js
@@ -1,4 +1,7 @@
 const serverless = require('serverless-http');
-const app = require('../server'); // Fixed path to go up one level to server.js
+const app = require('../server');
+
+// Forced Update: 2026-02-18-00-15
+console.log('--- Netlify Function Initialized ---');
 
 module.exports.handler = serverless(app);
